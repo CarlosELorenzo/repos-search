c0e9ad51d5e1b05b273177820c2ddf03849dfdca
CarlosELorenzo <ce.lorenzol@gmail.com>
2023-11-12 23:03:25 +0100

    feat: add no results and error messages components

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Typography } from "@mui/material";
+
+export const ErrorMessage = () => {
+  return (
+    <Typography variant="h2" align="center">
+      Something went wrong…
+    </Typography>
+  );
+};
---
+++ src/components/NoResultsMessage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Typography } from "@mui/material";
+
+export const NoResultsMessage = () => {
+  return (
+    <Typography variant="h2" align="center">
+      No results found…
+    </Typography>
+  );
+};
